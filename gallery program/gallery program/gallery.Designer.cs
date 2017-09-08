97791c4869edf531292e67c4b75a12665ca4fa11
theStoryEngine <holdingjupiter@gmail.com>
2017-09-07 21:00:10 -0700

    OP upload. Tell me what you think.
    Just click the exe in the first folder and you are on your way! Choose your folder and the slideshow starts immediately and is scrollable. I'd be happy to add any options if there is some interest.

---
+++ gallery program/gallery program/gallery.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace gallery_program
+{
+    partial class gallery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "choose an image folder";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(678, 396);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 8000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // gallery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 396);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "gallery";
+            this.Text = "SE Slideshow";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ gallery program/gallery program/gallery.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace gallery_program
+{
+    public partial class gallery : Form
+    {
+
+        List<string> images = new List<string>();
+        Random rnd = new Random();
+
+        public gallery()
+        {
+            InitializeComponent();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK) {
+
+                this.Activate();
+                this.BringToFront();
+                this.WindowState = FormWindowState.Maximized;
+                string filePath = folderBrowserDialog1.SelectedPath;
+                string[] extentions = { ".jpeg", ".jpg", ".JPG",  ".png" };
+                foreach (string file in Directory.EnumerateFiles(filePath, ".")
+                    .Where(s => extentions.Any(ext => ext == Path.GetExtension(s))))
+                {
+                    
+                    images.Add(file);
+
+                }
+                if (images.Count < 1) MessageBox.Show("There where no images to find!");
+                pictureBox1.Image = Image.FromFile(images[rnd.Next(images.Count)]);
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = Image.FromFile(images[rnd.Next(images.Count)]);
+
+        }
+    }
+}
